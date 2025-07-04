df8c0020b13e112848ea3a31c4661d7524511ec7
Matheesha Herath <matheeshadanindu@gmail.com>
2025-07-04 17:32:48 +0530

    reports dropdown

--- includes/header.php
+++ includes/header.php
@@ -9,11 +9,35 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="/csquare_erp/index.php">ERP System</a>
-        <div>
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="/csquare_erp/customer/list.php">Customers</a></li>
-                <li class="nav-item"><a class="nav-link" href="/csquare_erp/item/list.php">Items</a></li>
-                <li class="nav-item"><a class="nav-link" href="/csquare_erp/reports/invoice_report.php">Reports</a></li>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/csquare_erp/customer/list.php">Customers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/csquare_erp/item/list.php">Items</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        Reports
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="/csquare_erp/reports/invoice_report.php">Invoice Report</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/csquare_erp/reports/invoice_item_report.php">Invoice Item Report</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/csquare_erp/reports/item_report.php">Item Report</a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
